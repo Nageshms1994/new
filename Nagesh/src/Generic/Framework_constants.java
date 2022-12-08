22d7584a49287d79c780320082afe681ae8dc830
Nagesh <Nagesh@DESKTOP-MJD05I5>
2022-12-08 17:34:19 +0530

    java script

---
+++ Nagesh/src/Generic/Framework_constants.java
@@ -0,0 +1,12 @@
+package Generic;
+
+public class Framework_constants {
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./sf/geckodriver.exe";
+	 
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./sf/chromedriver.exe";
+	
+	String url="https://www.facebook.com";
+	
+}
